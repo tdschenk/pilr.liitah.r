0fe96ef826efde277ea10e06e76f581600e074ca
Tyler Schenk <schenk.tyler@gmail.com>
2015-07-01 16:51:43 -0500

    dash debug

--- R/liitah-dashboards.r
+++ R/liitah-dashboards.r
@@ -56,9 +56,8 @@ polls_per_day <- function(data, params, ...) {
   temp <- data.frame(day = substr(data$log$metadata$local_time, 0, 10))
   polls <- cbind(polls, temp)
   polls <- polls[polls$tag == "POLLING_SERVICE_ANDROID", ]
-  #polls <- polls[polls$category != "", ]
   days <- unique(polls$day)
-  stop(paste0("nrow:",nrow(polls), "    unique:", days[1]))
+  
   # Count polls per day
   summary <- data.frame(day = character(), category = character(), count = numeric())
   for (i in 1:length(days)) {
